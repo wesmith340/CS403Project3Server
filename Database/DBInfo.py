480f76b5477b42c0eb307c0ec6286954840fa30c
wesmith340 <35306192+wesmith340@users.noreply.github.com>
2021-11-29 18:30:27 -0500

    Version 1.4

--- Database/DBInfo.py
+++ Database/DBInfo.py
@@ -2,7 +2,7 @@ username = 'w97y1ayp039ur36m'
 password = 'fw9gwxxzkmofzfsa'
 server = 'dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306'
 database = 'mfvexsc1zrm922nb'
-SELECT_USER = """
+SELECT_ALL_USERS = """
     SELECT 
         User_TUID,
         Username,
@@ -10,6 +10,10 @@ SELECT_USER = """
         LastName
     FROM User 
 """
+CHECK_USER = SELECT_ALL_USERS + """WHERE Username = :username """
+VERIFY_USER = CHECK_USER + """AND Password = :password"""
+
+
 SELECT_MEETING = """
     SELECT 
         *
@@ -17,18 +21,25 @@ SELECT_MEETING = """
 """
 INSERT_USER = """
     INSERT INTO User (Username, FirstName, LastName, Password) 
+    VALUES(:username,:firstName,:lastName,:password)
 """
 DELETE_USER = """
-    DELETE FROM User 
+    DELETE FROM User WHERE Username = :username AND Password = :password
 """
+
 CREATE_MEETING = """
-    INSERT INTO TableTopGame (Organizer, GameName, MeetingDateTime, TotalOpenSlots, Latitude, Longitude) 
+    INSERT INTO TableTopGame (Organizer, GameName, MeetingDateTime, TotalOpenSlots, Latitude, Longitude)
+    VALUES(:organizer,:gameName,:meetingDateTime,:totalOpenSlots,:latitude,:longitude);
+    SET @last_id = LAST_INSERT_ID();
+    INSERT INTO Users_TableTopGame (User_TUID, TableTopGame_TUID) 
+    VALUES(:user,@last_id);
 """
 
 ATTEND_MEETING = """
     INSERT INTO Users_TableTopGame (User_TUID, TableTopGame_TUID) 
+    VALUES(:user,:meeting)
 """
 
 CHECK_ATTENDEE = """
-    SELECT * FROM Users_TableTopGame
+    SELECT * FROM Users_TableTopGame WHERE User_TUID=:user AND TableTopGame_TUID=:meeting
 """
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 # app.py
 from typing import Text
 from flask import Flask, json, request, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
+from sqlalchemy.sql.expression import bindparam
 from pybase64 import b64encode
 
 from Database import DBInfo
@@ -12,18 +13,15 @@ app = Flask(__name__)
 DBURL = f'mysql://{DBInfo.username}:{DBInfo.password}@{DBInfo.server}/{DBInfo.database}'
 engine = create_engine(DBURL)
 
-
-
-
 def verifyUser(username, password):
-    user = pd.read_sql(DBInfo.SELECT_USER+f"WHERE Username='{username}' AND Password='{password}'", engine)
+    user = pd.read_sql(text(DBInfo.VERIFY_USER).bindparams(username=username,password=password), engine)
     if len(user) > 0:
         return user.to_dict('records')[0]
     else:
         return False
 
 def notAttending(userTUID, meetingTUID):
-    attendee = pd.read_sql(DBInfo.CHECK_ATTENDEE+f"WHERE User_TUID='{userTUID}' AND TableTopGame_TUID='{meetingTUID}'", engine)
+    attendee = pd.read_sql(text(DBInfo.CHECK_ATTENDEE).bindparams(user=userTUID, meeting=meetingTUID), engine)
     if len(attendee) == 0:
         return True
     else:
@@ -41,15 +39,15 @@ def addUser():
     username = info['Username'].lower()
     fname = info['FirstName']
     lname = info['LastName']
-    hashPassword = b64encode(info['Password'])
+    hashPassword = b64encode(str.encode(info['Password']))
     row = None
     jsonMsg = jsonify({'success':False,'errorMsg':'Unknown error'})
     with engine.begin() as con:
-        row = con.execute(Text(DBInfo.SELECT_USER+f" WHERE Username = '{username}'")).fetchall()
+        row = con.execute(text(DBInfo.CHECK_USER).bindparams(username=username)).fetchall()
         
     if (row == None or len(row) == 0):
         with engine.begin() as con:
-            con.execute(DBInfo.INSERT_USER+f" VALUES('{username}','{fname}','{lname}','{hashPassword}')")
+            con.execute(text(DBInfo.INSERT_USER).bindparams(username=username,firstName=fname,lastName=lname,password=hashPassword))
             jsonMsg = jsonify({'success':True, 'msg':f'{username} has been added to the database'})
     else : 
         jsonMsg = jsonify({
@@ -69,7 +67,7 @@ def deleteUser():
 
     if (verifyUser(username, hashPassword)):
         with engine.begin() as con:
-            con.execute(DBInfo.DELETE_USER+f"WHERE Username='{username}' AND Password='{hashPassword}'")
+            con.execute(text(DBInfo.DELETE_USER).bindparams(username=username, password=hashPassword))
             jsonMsg = jsonify({'success':True, 'msg':f'{username} was successfully deleted'})
 
     return jsonMsg
@@ -78,7 +76,7 @@ def deleteUser():
 def createMeeting(username):
     print(request.json)
     info = request.json
-    hashPassword = b64encode(info['Password'])
+    hashPassword = b64encode(str.encode(info['Password']))
     gameName = info['gamename']
     meetingDateTime = info['datetime']
     totalOpenSlots = info['openslots']
@@ -91,10 +89,10 @@ def createMeeting(username):
     
     if user != False:
         with engine.begin() as con:
-            con.execute(DBInfo.CREATE_MEETING+f"VALUES('{user['User_TUID']}','{gameName}'," \
-                +f"'{meetingDateTime}','{totalOpenSlots}','{latitude}','{longitude}');" \
-                +"SET @last_id = LAST_INSERT_ID();" \
-                +DBInfo.ATTEND_MEETING+f"VALUES('{user['User_TUID']}',@last_id);")
+            con.execute(text(DBInfo.CREATE_MEETING).bindparams(
+                organizer=user['User_TUID'],gameName=gameName,meetingDateTime=meetingDateTime,
+                totalOpenSlots=totalOpenSlots,latitude=latitude,longitude=longitude, user=user['User_TUID']
+                ))
             jsonMsg = jsonify({'success':True, 'msg':f'Successfully added meeting'})
 
     return jsonMsg
@@ -108,7 +106,7 @@ def joinMeeting(username, meetingID):
 
     if user != False and notAttending(user['User_TUID'], meetingID):
         with engine.begin() as con:
-            con.execute(DBInfo.ATTEND_MEETING+f"VALUES({user['User_TUID']},{meetingID})")
+            con.execute(text(DBInfo.ATTEND_MEETING).bindparams(user=user['User_TUID'],meeting=meetingID))
             jsonMsg = jsonify({'success':True, 'msg':f'Successfully joined meeting'})
 
     return jsonMsg
@@ -118,11 +116,11 @@ def joinMeeting(username, meetingID):
 @app.route('/loginuser/<username>', methods=['POST'])
 def loginUser(username):
     info = request.json
-    hashPassword = b64encode(info['Password'])
+    hashPassword = b64encode(str.encode(info['Password']))
     row = None
     jsonMsg = jsonify({'success':False, 'errorMsg':'Username and Password do not match'})
     with engine.begin() as con:
-        row = con.execute(Text(DBInfo.VERIFY_USER+f" WHERE Username = '{username}' AND Password = '{hashPassword}'")).fetchall()
+        row = con.execute(text(DBInfo.VERIFY_USER).bindparams(username=username,password=hashPassword)).fetchall()
     if len(row) > 0:
         jsonMsg = jsonify({'success':True,'msg':'Username and Password are in the database'})
     return jsonMsg
