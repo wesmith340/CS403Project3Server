2314b7430d89e4c36486cf3eead3aac32f52c238
wesmith340 <35306192+wesmith340@users.noreply.github.com>
2021-12-01 14:10:20 -0500

    Update

--- dbinit.sql
+++ dbinit.sql
@@ -13,6 +13,7 @@ CREATE TABLE TableTopGame (
 	TableTopGame_TUID INT NOT NULL AUTO_INCREMENT,
 	Organizer INT NOT NULL,
 	GameName VARCHAR(64),
+	GameCategory VARCHAR(64),
 	MeetingDateTime DateTime,
 	TotalOpenSlots INT,
 	Latitude Double,
@@ -27,3 +28,28 @@ CREATE TABLE Users_TableTopGame (
 	TableTopGame_TUID INT REFERENCES TableTopGame (TableTopGame_TUID),
 	PRIMARY KEY(Users_TableTopGame_TUID)
 );
+
+DROP TABLE if EXISTS GameCategories;
+CREATE TABLE GameCategories (
+	GameCategories_TUID INT NOT NULL AUTO_INCREMENT,
+	Category VARCHAR(64),
+	PRIMARY KEY(GameCategories_TUID)
+);
+
+INSERT INTO GameCategories (Category) VALUES
+('Board game'),
+('Card game'),
+('Dice game'),
+('Paper and pencil'),
+('Tabletop role-playing'),
+('Strategy'),
+('Tile-based');
+
+DROP TABLE if EXISTS TableTopGame_GameCategories;
+CREATE TABLE TableTopGame_GameCategories(
+	TableTopGame_GameCategories_TUID INT NOT NULL,
+	TableTopGame_TUID INT REFERENCES TableTopGame (TableTopGame_TUID),
+	GameCategories_TUID INT REFERENCES GameCategories (GameCategories_TUID), 
+	PRIMARY KEY(TableTopGame_GameCategories_TUID)
+);
+
